8f16f1c0eccfe8f1464647801d2104b215e2c95b
Phil Gates-Shannon <phil.gs@gmail.com>
2017-02-14 07:36:48 -0500

    [WIP] Add default compare function

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let moment = require( 'moment' );
+let _ = require( 'lodash' );    // TODO Only require necessary modules
 
 const BEFORE = 'before';
 const MOMENT = 'moment';
@@ -22,13 +23,33 @@ module.exports = function( chai, utils ) {
         utils.flag( this, namespace( BEFORE ), true );
     } );
 
-    Assertion.addMethod( MOMENT, function( timestamp ) {
+    Assertion.addMethod( MOMENT, function( timestamp, accuracy ) {
         this.assert(
             moment.isDate( timestamp ) || moment.isMoment( timestamp ),
             'expected #{exp} to be a Date or Moment, but it is #{act}',
             'this should not be used $F119',
             timestamp,
             typeof timestamp
+        );
+
+        let obj = moment.isMoment( this._obj ) ? this._obj : moment( this._obj );
+        timestamp = moment.isMoment( timestamp ) ? timestamp : moment( timestamp );
+
+        // Create a curried comparison function, to reduce redundancy
+        let compare = null;
+        if ( accuracy ) {
+            compare = _.curry( obj.isSame.bind( obj ), 1 )( _, accuracy );
+        } else {
+            compare = _.curry( obj.isSame.bind( obj ), 1 );
+        }
+
+        // Do the comparison
+        this.assert(
+            compare( timestamp ),
+            'expected #{exp} to be the same as {#act}',
+            'expected #{exp} to not be the same as {#act}',
+            obj,
+            timestamp
         );
     } );
 };
--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
 let chai = require( 'chai' );
 let chaiMoment = require( '../index' );
 let dirtyChai = require( 'dirty-chai' );
+let moment = require( 'moment' );
 // let path = require( 'path' );
 // let _ = require( 'lodash' );
 
@@ -24,7 +25,21 @@ describe( 'chai-moment', function() {
                 + 'be a Date or Moment, but it is \'string\'' );
         } );
 
-        it( 'performs an is-same comparison if no flag is set' );
+        it( 'performs an is-same comparison if no flag is set', function() {
+            let m1 = moment( '2005-08-13' );
+            let m2 = moment( '2005-08-13' );
+            let m3 = moment( '2008-01-20' );
+            let m4 = moment( 1487070166773 );
+            let m5 = moment( 1487070166000 );
+
+            // Test "pass" condition
+            // expect( m1 ).is.moment( m2 );
+            expect( m4 ).is.not.moment( m5 );
+            // expect( m4 ).is.moment( m5, 'second' );
+
+            // Test "fail" condition
+        } );
+
         it( 'accepts an optional second parameter to control the specificity of the comparison' );
 
     } );
